454bb2fa779d7224e7114694cb1403bf9ddd6127
Tadowino <tadowino254@gmail.com>
2024-05-22 11:41:34 +0300

    form-generator created form with inputs and download button

--- meme-Generator/src/app/app.module.ts
+++ meme-Generator/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ColorChromeModule } from 'ngx-color/chrome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GeneratorComponent } from './generator/generator.component';
@@ -14,7 +14,8 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ColorChromeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
