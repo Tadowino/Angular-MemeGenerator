c64b252142e79c755ba4a0ffa436ccc53d9fbb16
Tadowino <tadowino254@gmail.com>
2024-05-26 13:24:09 +0300

    preview image select image from input and display on canvas

--- meme-Generator/src/app/generator/generator.component.ts
+++ meme-Generator/src/app/generator/generator.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-generator',
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrl: './generator.component.css'
 })
 export class GeneratorComponent {
+//Select canvas
+@ViewChild('memeCanvas', {static: false})myCanvas:any;
+
+preview(e:any){
+  //capturing canvas
+  let canvas = this.myCanvas.nativeElement;
+  //preparing the canvas for drawing
+  let ctx = canvas.getContext('2d');
+  //create an instance of  filereader
+  let render = new FileReader();
+  //read specific file directly from its location
+  render.readAsDataURL(e.target.files[0]);
+
+  render.onload = function(event:any){
+    //create an image
+    const img = new Image();
+    //assingn immage to file data
+    img.src = event.target.result as string;
+    img.onload = function(){
+      //draw image
+      ctx.drawImage(img, 60, 50 , 700, 800);
+    } 
+
+  }
+
+
+}
 
 }
